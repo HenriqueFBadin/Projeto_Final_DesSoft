61235f93b319193ed0def849ee57577adc27cfcd
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2022-05-18 16:14:52 -0300

    Criação da tela

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 print("Criadores do projeto: Eduardo Selber Castanho, Henrique Fazzio Badin, Henrique Rocha Bomfim")
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((1920, 1080))
+pygame.display.set_caption('Mortal Insper!')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
