5a30f94c31c095160e1341682f4ca6883a021160
Henrique <henrique.badin@gmail.com>
2022-05-26 11:26:56 -0300

    Organização e correção do punch com função

--- classes.py
+++ classes.py
@@ -9,7 +9,7 @@ p1_shoot=False
 p2_shoot=False
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, orientacao, imgs, img_weaks, all_sprites, all_powers, sprite_power, power_img):
+    def __init__(self, orientacao, imgs, img_weaks, all_sprites, all_powers, sprite_power, power_img, sprite_punch, punch_img, goldpunch_img, shoot_img, goldshot_img):
         # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
 
@@ -25,20 +25,27 @@ class Player(pygame.sprite.Sprite):
         self.speedx = 0
         self.speedy = 0
         self.energy = 0
+        self.punching_energy=0
+        self.shooting_energy=0
         self.is_jumping = False
         self.is_punching = False
+        self.is_shooting=False
         self.life=100
         self.all_sprites=all_sprites
         self.all_powers=all_powers
         self.sprite_power=sprite_power
+        self.sprite_punch=sprite_punch
         self.power_img=power_img
+        self.punch_img=punch_img
+        self.goldpunch_img=goldpunch_img
+        self.shoot_img=shoot_img
+        self.goldshot_img=goldshot_img
         self.damage = 10
+
     def update(self):
         # Atualização da posição do player
         if self.is_jumping:
             self.energy += 1
-        if self.is_punching:
-            self.duracao -= 1
         self.rect.x += self.speedx
         self.rect.y += self.energy
         if self.life <= 50:
@@ -60,19 +67,48 @@ class Player(pygame.sprite.Sprite):
         if self.life>=50:
             self.image = self.imgs[self.orientacao]
             self.image_weak = self.img_weaks[self.orientacao]
-    
+        
+        if self.is_punching and self.life>=50:
+            self.punching_energy-=1
+            self.image = self.punch_img[self.orientacao]
+            if self.punching_energy<=0:
+                self.is_punching=False
+
+        if self.is_punching and self.life<50:
+            self.punching_energy-=1
+            self.image = self.goldpunch_img[self.orientacao]
+            if self.punching_energy<=0:
+                self.is_punching=False
+
+        if self.is_shooting and self.life>=50:
+            self.shooting_energy-=1
+            self.image = self.shoot_img[self.orientacao]
+            if self.shooting_energy<=0:
+                self.is_shooting=False
+        if self.is_shooting and self.life<50:
+            self.shooting_energy-=1
+            self.image = self.goldshot_img[self.orientacao]
+            if self.shooting_energy<=0:
+                self.is_shooting=False
+
     def jump(self):
         if not self.is_jumping:
             self.energy = -25
             self.is_jumping = True
 
     def shoot(self):
-        # A nova bala vai ser criada logo acima e no centro horizontal da nave
+
+        self.is_shooting=True
+        self.shooting_energy=12
         new_power=Power(self.orientacao, [self.power_img, self.power_img], self.rect.bottom-85, self.rect.centerx)
         self.all_sprites.add(new_power)
         self.all_powers.add(new_power)
         self.sprite_power.add(new_power)
-    
+
+    def punch(self):
+        if not self.is_punching:
+            self.punching_energy = 12
+            self.is_punching=True
 
 class Power(pygame.sprite.Sprite):
 
@@ -90,9 +126,10 @@ class Power(pygame.sprite.Sprite):
             self.rect.centerx = centerx
             self.rect.bottom = bottom
             self.speedx = -10  # Velocidade fixa para a horizontal]
-            self.orientacao = orientacao
+
 
         def update(self):
+            
             # O poder só se move no eixo x:
             if self.orientacao==0:
                 self.rect.centerx+=self.speedx
--- main.py
+++ main.py
@@ -25,29 +25,25 @@ pygame.display.set_caption('Mortal Insper!')
 
 # ----- Inicia estruturas de dados
 game = True
-quadrado1_img=pygame.image.load('Imagem/Quadrado_Teste.png').convert_alpha()
-quadrado2_img=pygame.image.load('Imagem/Quadrado_Teste_2.png').convert_alpha()
-humberto_img=pygame.image.load('Imagem/HUmberto-ryu0.png').convert_alpha()
-p1socando_img = pygame.image.load('Imagem/Quadrado_Teste_socando.png').convert_alpha()
-p2socando_img = pygame.image.load('Imagem/hu soco.png').convert_alpha()
-p2haduken_img = pygame.image.load('Imagem/humberto haduken.png').convert_alpha()
-humbertogold_img=pygame.image.load('Imagem/hugold.png').convert_alpha()
-humbertogoldsoco_img=pygame.image.load('Imagem/hugold soco.png').convert_alpha()
-werewolf_img = pygame.image.load('Imagem/werewolf.png').convert_alpha()
-werewolf_golden_img = pygame.image.load('Imagem/werewolf_golden.png').convert_alpha()
+pl_humb_img=pygame.image.load('Imagem/HUmberto-ryu0.png').convert_alpha()
+sr_Teste_img = pygame.image.load('Imagem/Quadrado_Teste_socando.png').convert_alpha()
+sl_humb_img = pygame.image.load('Imagem/hu soco.png').convert_alpha()
+al_humb_img = pygame.image.load('Imagem/humberto haduken.png').convert_alpha()
+pl_humb2_img=pygame.image.load('Imagem/hugold.png').convert_alpha()
+sl_humb2_img=pygame.image.load('Imagem/hugold soco.png').convert_alpha()
+pr_wolf_img = pygame.image.load('Imagem/werewolf.png').convert_alpha()
+pr_wolf2_img = pygame.image.load('Imagem/werewolf_golden.png').convert_alpha()
 power_img=pygame.image.load('Imagem/haduken.png').convert_alpha()
 power2_img=pygame.image.load('Imagem/hadukenfogo.png').convert_alpha()
 background_img = pygame.image.load('Imagem/cenário.jpg').convert_alpha()
-quadrado1_img=pygame.transform.scale(quadrado1_img, (P1_WIDTH, P1_HEIGHT))
-humberto_img=pygame.transform.scale(humberto_img, (P1_WIDTH, P1_HEIGHT+30))
-quadrado2_img=pygame.transform.scale(quadrado2_img, (P1_WIDTH, P1_HEIGHT))
-werewolf_img=pygame.transform.scale(werewolf_img, (P1_WIDTH, P1_HEIGHT+30))
-werewolf_golden_img=pygame.transform.scale(werewolf_golden_img, (P1_WIDTH, P1_HEIGHT+30))
-p1socando_img=pygame.transform.scale(p1socando_img, (200, P1_HEIGHT))
-p2socando_img=pygame.transform.scale(p2socando_img, (200, P1_HEIGHT+30))
-p2haduken_img=pygame.transform.scale(p2haduken_img, (200, P1_HEIGHT+30))
-humbertogold_img=pygame.transform.scale(humbertogold_img, (180, P1_HEIGHT+30))
-humbertogoldsoco_img=pygame.transform.scale(humbertogoldsoco_img, (180, P1_HEIGHT+30))
+pl_humb_img=pygame.transform.scale(pl_humb_img, (P1_WIDTH, P1_HEIGHT+30))
+pr_wolf_img=pygame.transform.scale(pr_wolf_img, (P1_WIDTH, P1_HEIGHT+30))
+pr_wolf2_img=pygame.transform.scale(pr_wolf2_img, (P1_WIDTH, P1_HEIGHT+30))
+sr_Teste_img=pygame.transform.scale(sr_Teste_img, (200, P1_HEIGHT))
+sl_humb_img=pygame.transform.scale(sl_humb_img, (200, P1_HEIGHT+30))
+al_humb_img=pygame.transform.scale(al_humb_img, (200, P1_HEIGHT+30))
+pl_humb2_img=pygame.transform.scale(pl_humb2_img, (180, P1_HEIGHT+30))
+sl_humb2_img=pygame.transform.scale(sl_humb2_img, (180, P1_HEIGHT+30))
 pr_dio_img=pygame.image.load('Imagem/DIO_Parado_Direita.png')
 pl_dio_img=pygame.image.load('Imagem/DIO_Parado_Esquerda.png')
 pr_dio_img=pygame.transform.scale(pr_dio_img, (180, P1_HEIGHT+30))
@@ -59,9 +55,6 @@ background_img=pygame.transform.scale(background_img, (WIDTH, HEIGHT))
 # ----- Controle de FPS e Tick Rate
 clock = pygame.time.Clock()
 tick_rate = 64
-#parametro pro jump:
-isjump=False
-jumpcount=1
 
 # ----- Agrupando as sprites
 all_sprites = pygame.sprite.Group()
@@ -70,11 +63,12 @@ sprite_p1= pygame.sprite.Group()
 sprite_p2= pygame.sprite.Group()
 sprite_power1=pygame.sprite.Group()
 sprite_power2=pygame.sprite.Group()
+sprite_punch=pygame.sprite.Group()
 
 # ----- Definindo os Players 
 
-player1 = Player(1, [pl_dio_img, pr_dio_img], [werewolf_golden_img, werewolf_golden_img], all_sprites, all_powers, sprite_power1, power_img)
-player2 = Player(0, [humberto_img, humberto_img], [humbertogold_img, humbertogold_img], all_sprites, all_powers, sprite_power2, power2_img)
+player1 = Player(1, [pl_dio_img, pr_dio_img], [pr_wolf2_img, pr_wolf2_img], all_sprites, all_powers, sprite_power1, power_img, sprite_punch, [sr_Teste_img, sr_Teste_img], [sl_humb2_img, sl_humb2_img], [al_humb_img, al_humb_img], [al_humb_img, al_humb_img])
+player2 = Player(0, [pl_humb_img, pl_humb_img], [pl_humb2_img, pl_humb2_img], all_sprites, all_powers, sprite_power2, power2_img, sprite_punch, [sl_humb_img, sl_humb_img], [sl_humb2_img, sl_humb2_img], [al_humb_img, al_humb_img], [al_humb_img, al_humb_img])
 all_sprites.add(player1)
 all_sprites.add(player2)
 sprite_p1.add(player1)
@@ -105,7 +99,6 @@ while game:
                 player2.speedx += 8
             if event.key == pygame.K_UP:
                 player2.jump()
-                player2.speedy -= 8
 
 # ----------Movimentação do player 1 (W,A,D):
             if event.key == pygame.K_a:
@@ -116,24 +109,18 @@ while game:
                 player1.speedx += 8
             if event.key==pygame.K_w:
                 player1.jump()
-                player1.speedy -= 8
 
 # ----------Tiros (Q, L):            
             if event.key==pygame.K_q and player1.life > 0:
                 player1.shoot()
             if event.key==pygame.K_l and player2.life > 0:
                 player2.shoot()
-                player2.image=p2haduken_img
-           
 
 # ----------Sprite do soco (E, <):
             if event.key == pygame.K_e:
-                player1.image = p1socando_img
+                player1.punch()
             if event.key == pygame.K_COMMA:
-                if player2.image==humbertogold_img:
-                    player2.image=humbertogoldsoco_img
-                else:
-                    player2.image = p2socando_img
+                player2.punch()
                 
         
         # Verifica se soltou alguma tecla.
@@ -151,16 +138,7 @@ while game:
                 player2.jump()
 
     # ----- Atualiza estado do jogo
-    if contador_soco_1 > 0 and player1.image == p1socando_img:
-        contador_soco_1 -= 1
-    elif contador_soco_1 <= 0:
-        player1.image = werewolf_img
-        contador_soco_1 = 10
-    if contador_soco_2 > 0 and player2.image == p2socando_img:
-        contador_soco_2 -= 1
-    elif contador_soco_2 <= 0:
-        player2.image = humberto_img
-        contador_soco_2 = 10
+    
     #hits = pygame.sprite.spritecollideany(player1, player2, True)
     # Atualizando a situação dos players
     all_sprites.update()
@@ -194,8 +172,9 @@ while game:
     hit1=pygame.sprite.spritecollide(player2, sprite_p1, death1)
     hit2=pygame.sprite.spritecollide(player1, sprite_p2, death2)
 
+########################TOMAR CUIDADO! AS IMAGENS N SÃO VARIAVEIS, MUDAR NA HORA QUE TIVER + DE 2 PERSONAGENS PARA N DAR CONFLITO#####################  
     if hit1:
-        if player1.image == p1socando_img:
+        if player1.image == sr_Teste_img or player1.image==sl_humb2_img:
             player2.life -= player1.damage
         if player2.rect.centerx >= player1.rect.centerx:
             player2.rect.x += 30
@@ -206,7 +185,7 @@ while game:
             death2=True
 
     if hit2:
-        if player2.image == p2socando_img:
+        if player2.image == sl_humb_img or player2.image==sl_humb2_img:
             player1.life -= player2.damage
             player2.rect.centerx += 50
         if player1.rect.centerx >= player2.rect.centerx:
