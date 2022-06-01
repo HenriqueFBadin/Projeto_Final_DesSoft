bbdd9e7b633a4e4d7beb3ff84e09842f0cba0de8
Selber_codes <eduardosc6@al.insper.edu.br>
2022-06-01 17:48:13 -0300

    inicio do meu de seleção

---
+++ character_selection.py
@@ -0,0 +1,38 @@
+import pygame
+from assets import *
+QUIT=2
+GAME=3
+INIT=1
+state=1
+player1_selecionou=False
+player2_selecionou=False
+def character_selection(screen):
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+
+    # Carrega o fundo da tela inicial
+    background = pygame.image.load('character selection menu.png').convert()
+    background_rect = background.get_rect()
+
+    running = True
+    while running:
+
+        # Ajusta a velocidade do jogo.
+        clock.tick(tick_rate)
+
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            if event==pygame.MOUSEMOTION:
+                if event==pygame.MOUSEBUTTONDOWN:
+                    mouse_pousition=pygame.mouse.get.pos()
+
+
+        # A cada loop, redesenha o fundo e os sprites
+        screen.blit(background, background_rect)
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+        state=4
+        running =False
+
+    return state
